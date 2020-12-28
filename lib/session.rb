47b4f2fbc232be67583fe6f76fad1c522afcf6a4
joshIsCoding <yes_you_do@hotmail.com>
2020-12-28 20:13:27 +0000

    Add key Session methods

--- lib/session.rb
+++ lib/session.rb
@@ -1,19 +1,25 @@
 require 'json'
 
 class Session
+  COOKIE_NAME = '_rails_lite_app'
   # find the cookie for this app
   # deserialize the cookie into a hash
   def initialize(req)
+    cookie_val = req.cookies[COOKIE_NAME]
+    @cookie = cookie_val ? JSON.parse( cookie_val ) : {}
   end
 
   def [](key)
+    @cookie[key]
   end
 
   def []=(key, val)
+    @cookie[key] = val
   end
 
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.set_cookie( COOKIE_NAME, @cookie.to_json )
   end
 end
