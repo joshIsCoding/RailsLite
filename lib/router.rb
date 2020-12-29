271f94c0f9b34301f0ee96fe9b387b09b1543440
joshIsCoding <yes_you_do@hotmail.com>
2020-12-29 23:58:08 +0000

    Add #add_route, #initialize, #match and http_methods

--- lib/router.rb
+++ lib/router.rb
@@ -2,15 +2,20 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern, @http_method, @controller_class = pattern, http_method, controller_class
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    req.request_method == http_method.to_s.upcase && @pattern.match?( req.path )
   end
 
   # use pattern to pull out route params (save for later?)
   # instantiate controller and call controller action
   def run(req, res)
+    controller = controller_class.new( req, res, {} )
+    controller.invoke_action( action_name )
   end
 end
 
@@ -18,10 +23,12 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new( pattern, method, controller_class, action_name )
   end
 
   # evaluate the proc in the context of the instance
@@ -32,10 +39,14 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method( http_method ) do |pattern, controller, action|
+      add_route( pattern, http_method, controller, action )
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    routes.find { |r| r.matches?( req ) }
   end
 
   # either throw 404 or call run on a matched route
