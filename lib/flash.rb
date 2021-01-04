5ed346d63673178dc24e158334c8f6118900cf9d
joshIsCoding <yes_you_do@hotmail.com>
2021-01-04 21:34:30 +0000

    Create Flash skeleton

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,4 +1,31 @@
 require 'json'
 
 class Flash
+  attr_reader :stored, :current
+
+  COOKIE_NAME = '_rails_lite_app_flash'
+
+  def initialize( req )
+    @stored = extract_flash_cookie( req )
+    @current = {}
+  end
+
+  def []=( key, val )
+
+  end
+
+  def now
+  end
+
+  def []( key )
+  end
+
+  def store_flash( res )
+  end
+
+  private
+
+  def extract_flash_cookie
+  end
+
 end
