6adaf5b627703e47ff3a64301b83e44333552a67
joshIsCoding <yes_you_do@hotmail.com>
2021-01-04 21:53:43 +0000

    Add all spec'd flash methods, plus private helper

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,31 +1,34 @@
 require 'json'
 
 class Flash
-  attr_reader :stored, :current
+  attr_reader :current, :future
 
   COOKIE_NAME = '_rails_lite_app_flash'
 
   def initialize( req )
-    @stored = extract_flash_cookie( req )
-    @current = {}
+    @current = extract_flash_cookie( req )
+    @future = {}
   end
 
   def []=( key, val )
-
+    @future[key.to_s] = val
   end
 
   def now
+    @current
   end
 
   def []( key )
+    future.merge( current )[key.to_s]
   end
 
   def store_flash( res )
+    res.set_cookie( COOKIE_NAME, { path: '/', value: future.to_json } )
   end
 
   private
 
-  def extract_flash_cookie
+  def extract_flash_cookie( req )
+    req.cookies[COOKIE_NAME] ? JSON.parse( req.cookies[COOKIE_NAME] ) : {}
   end
-
 end
