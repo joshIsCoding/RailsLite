19828b5aaf047b803c8a188a23106f5569851884
joshIsCoding <yes_you_do@hotmail.com>
2021-01-11 23:59:28 +0000

    Add #finish to returned rack::response; add exception rendering

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -11,12 +11,19 @@ class ShowExceptions
   rescue => e
     return Rack::Response.new( 
       render_exception( e ), 500, {'Content-Type' => 'text/html'}
-    )
+    ).finish
   end
 
   private
 
   def render_exception( e )
+    @e = e
+    template = ERB.new( File.read( rescue_template_path ) )
+    template.result( binding )
+  end
+
+  def rescue_template_path
+    File.join( File.dirname( __FILE__ ), 'templates/rescue.html.erb' )
   end
 
 end
