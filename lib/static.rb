37bcff882a7bee29dbe7167201fb31c03f65fd2a
joshIsCoding <yes_you_do@hotmail.com>
2021-01-12 23:53:31 +0000

    initialise basic #call, #full_path and path const

--- lib/static.rb
+++ lib/static.rb
@@ -1,7 +1,19 @@
 class Static
+  PUBLIC_PATH = '/public'
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    req = Rack::Request.new( env )
+    if req.path.starts_with?( PUBLIC_PATH )
+      serve_asset( req.path )
+    else
+      @app.call( env )
+    end
+  end
+
+  def full_path( file_path )
+    File.absolute_path( file_path, PUBLIC_PATH )
   end
 end
