60daec73eec15b3b6633bd4d6b2491d7e5aa4a8b
joshIsCoding <yes_you_do@hotmail.com>
2021-01-13 23:51:25 +0000

    Expand and refactor Static

--- lib/static.rb
+++ lib/static.rb
@@ -1,19 +1,53 @@
 class Static
   PUBLIC_PATH = '/public'
+
+  attr_reader :full_path, :path
+
   def initialize(app)
     @app = app
   end
 
   def call(env)
     req = Rack::Request.new( env )
-    if req.path.starts_with?( PUBLIC_PATH )
-      serve_asset( req.path )
+
+    if static_asset_request?( req )
+      set_path( req.path )
+      build_asset_response
     else
       @app.call( env )
     end
   end
 
-  def full_path( file_path )
-    File.absolute_path( file_path, PUBLIC_PATH )
+  def build_asset_response
+    if File.exist?( full_path )
+      serve_asset
+    else
+      Rack::Response.new(
+        "No such file found: #{path}", 404, {'Content-Type' => 'text/html'}
+      ).finish
+    end
+
+  end
+
+  def serve_asset
+    res = Rack::Response.new
+    res.write( File.read( full_path ) )
+    res['Content-Type'] = Rack::Mime.mime_type( extract_ext )
+    res.finish
+  end
+
+  def static_asset_request?( req )
+    req.path.start_with?( PUBLIC_PATH )
+  end
+
+  private
+
+  def extract_ext
+    ext = path.split( '.' ).last
+  end
+
+  def set_path( path )
+    @path = path
+    @full_path = File.absolute_path( ".#{path}" )
   end
 end
