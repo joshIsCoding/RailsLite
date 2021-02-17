172d10fb7a90e0bcf593cf96660d2233fe7baf49
joshIsCoding <yes_you_do@hotmail.com>
2021-02-17 23:58:18 +0000

    Fill in link_to method with basic text, href & class assignment

--- lib/action_view_lite/view_methods.rb
+++ lib/action_view_lite/view_methods.rb
@@ -1,5 +1,9 @@
 module ViewMethods
   def link_to( link_text, href, **opts )
-    
+    <<~HTML
+      <a href="#{href}" class="opts[:class]">
+        #{link_text}
+      </a>
+    HTML
   end
 end
