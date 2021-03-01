b50cc1fcdc9745ff6f4180f078e132a86764247c
joshIsCoding <yes_you_do@hotmail.com>
2021-03-01 23:59:51 +0000

    Implement FlashNow

--- lib/flash.rb
+++ lib/flash.rb
@@ -1,34 +1,50 @@
 require 'json'
 
-class Flash
-  attr_reader :current, :future
-
+module ActionDispatchLite
   COOKIE_NAME = '_rails_lite_app_flash'
 
-  def initialize( req )
-    @current = extract_flash_cookie( req )
-    @future = {}
+  class Flash
+    attr_reader :now, :future
+
+
+    def initialize( req )
+      @now = FlashNow.new( req )
+      @future = {}
+    end
+
+    def []=( key, val )
+      debugger
+      @future[key.to_s] = val
+    end
+
+    def []( key )
+      future[key.to_s] || now[key.to_s]
+    end
+
+    def store_flash( res )
+      res.set_cookie( COOKIE_NAME, { path: '/', value: future.to_json } )
+    end
   end
 
-  def []=( key, val )
-    @future[key.to_s] = val
-  end
+  def FlashNow
 
-  def now
-    @current
-  end
+    def initialize( req )
+      @now = extract_flash_cookie( req )
+    end
 
-  def []( key )
-    future.merge( current )[key.to_s]
-  end
+    def []=( k, v )
+      @now[k.to_s] = v
+    end
 
-  def store_flash( res )
-    res.set_cookie( COOKIE_NAME, { path: '/', value: future.to_json } )
-  end
+    def []( k )
+      @now[k.to_s]
+    end
 
-  private
+    private
 
-  def extract_flash_cookie( req )
-    req.cookies[COOKIE_NAME] ? JSON.parse( req.cookies[COOKIE_NAME] ) : {}
+    def extract_flash_cookie( req )
+      req.cookies[COOKIE_NAME] ? JSON.parse( req.cookies[COOKIE_NAME] ) : {}
+    end
+
   end
 end
