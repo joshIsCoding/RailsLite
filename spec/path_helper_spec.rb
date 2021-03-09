d14701647b1b410b7e7c7c07960254926654eb4a
joshIsCoding <yes_you_do@hotmail.com>
2021-03-09 21:25:38 +0000

    Add #rest_type specs

--- spec/path_helper_spec.rb
+++ spec/path_helper_spec.rb
@@ -1,10 +1,34 @@
 require 'router'
+require 'byebug'
 require './lib/action_dispatch_lite/path_helper'
 
 describe ActionDispatchLite::PathHelper do
   subject( :path_helper ) { described_class.new( route )}
 
   let( :route ) { Route.new( Regexp.new('^/users$'), :get, 'CakeOfTheDay', :show ) }
+  
+  describe '#rest_type' do
+    let( :route ) { Route.new( Regexp.new( path ), :get, 'CakeOfTheDay', :wow ) }
+
+    { edit: '/edit', new: '/new', id: '/:id' }.each do |sym, suffix|
+
+      context "when the path ends with '#{suffix}'" do
+        let( :path ) { "^/cakes#{suffix}$" }
+
+        it "is set to :#{sym} on init" do
+          expect( path_helper.rest_type ).to eq( sym == :id ? :resource : sym )
+        end
+      end
+    end
+
+    context "when the path isn't recognised as an edit, new or resource path" do
+      let( :path ) { "^/cakes$" }
+
+      it 'is set to :resources on init' do
+        expect( path_helper.rest_type ).to eq( :resources )
+      end
+    end
+  end
 
   describe '#static_segments' do
     it 'returns the static segment of a simple route path as an array of substrings' do
