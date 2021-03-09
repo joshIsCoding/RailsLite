f51d82e2b03f540550713e8e3bf4d6ae8de1d7a7
joshIsCoding <yes_you_do@hotmail.com>
2021-03-09 21:25:26 +0000

    Introduce rest_type on init; refactor #static_segments

--- lib/action_dispatch_lite/path_helper.rb
+++ lib/action_dispatch_lite/path_helper.rb
@@ -2,20 +2,36 @@ module ActionDispatchLite
   class PathHelper
     PATH_SUFFIXES = %w[ edit new ]
 
-    attr_reader :route, :pattern
+    attr_reader :route, :pattern, :rest_type
 
     def initialize( route )
       @route = route
-      @pattern = route.pattern
+      @pattern = stringify_pattern( route.pattern )
+      @rest_type = infer_rest_type
     end
 
-    
-
     def static_segments
-      pattern_str = pattern.is_a?( String ) ? pattern : pattern.inspect
-      PATH_SUFFIXES.reduce( pattern_str ) { |pattern_str,suffix| pattern_str.chomp( "/#{suffix}" ) }
+      pattern.chomp( '/new' ).chomp( '/edit' )
                    .scan( /\/(\w+)/ )
                    .flatten
+    end
+
+    private
+
+    def stringify_pattern( pattern )
+      pattern.is_a?( String ) ? pattern : pattern.inspect.chomp( '$/' ).delete_prefix( '/^' )
+    end
+
+    def infer_rest_type
+      if pattern.end_with?( '/edit' )
+        :edit
+      elsif pattern.end_with?( "/new" )
+        :new
+      elsif pattern.end_with?( "/:id" )
+        :resource
+      else
+        :resources
+      end
     end
   end
 end
