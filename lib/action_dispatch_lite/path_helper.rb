062989301117d089fae70f0a0b907d2e2f0ce367
joshIsCoding <yes_you_do@hotmail.com>
2021-03-09 20:42:25 +0000

    Restructure nested PathUtilities as class

--- lib/action_dispatch_lite/path_helper.rb
+++ lib/action_dispatch_lite/path_helper.rb
@@ -1,10 +1,19 @@
 module ActionDispatchLite
-  module PathUtilities
+  class PathHelper
     PATH_SUFFIXES = %w[ edit new ]
 
-    def static_segments( pattern )
-      pattern = pattern.is_a?( String ) ? pattern : pattern.inspect
-      PATH_SUFFIXES.reduce( pattern ) { |pattern,suffix| pattern.chomp( "/#{suffix}" ) }
+    attr_reader :route, :pattern
+
+    def initialize( route )
+      @route = route
+      @pattern = route.pattern
+    end
+
+    
+
+    def static_segments
+      pattern_str = pattern.is_a?( String ) ? pattern : pattern.inspect
+      PATH_SUFFIXES.reduce( pattern_str ) { |pattern_str,suffix| pattern_str.chomp( "/#{suffix}" ) }
                    .scan( /\/(\w+)/ )
                    .flatten
     end
